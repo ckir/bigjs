884e9b126a81b78204b660b65bd98d3c843c4019
Costas Kirgoussios <ckir1508@hotmail.com>
2023-03-27 18:37:01 +0300

    Update index

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
-export {LargePrint} from './src/lib/print-fonts.mjs'
+export {FontsPrint} from './src/lib/print-fonts.mjs'
 export {FigletPrint} from './src/lib/print-figlet.mjs'
 export {BigBlessed} from './src/blessed/index.mjs'
